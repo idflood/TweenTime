f61bdb22c5f32ab6028091ffa7a60a5a689dfb37
David Mignot <david.mig@gmail.com>
2014-12-22 10:26:23 +0100

    don't output chunks informations in terminal

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('scripts', function(cb) {
   }, function(err, stats) {
     if(err) throw new gutil.PluginError("webpack", err);
     gutil.log("[webpack]", stats.toString({
-        // output options
+        chunks: false
     }));
     cb();
   });
