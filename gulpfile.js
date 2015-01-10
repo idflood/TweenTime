4ae3345333570684c8ca6dbaa04d1e09d06fc00b
David Mignot <david.mig@gmail.com>
2015-01-10 10:15:06 +0100

    change how jslint task is run in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task('scripts', function(cb) {
   });
 });
 
-gulp.task('scripts:dist', function(cb) {
+gulp.task('scripts:dist', ['lint-scripts'], function(cb) {
   var conf = getWebpackConfig();
   // Add .min.js to output filename.
   conf.output.filename = "TweenTime.[name].min.js";
@@ -140,8 +140,8 @@ gulp.task('watch', function() {
   gulp.watch(['examples/*.html'], livereload.changed);
   gulp.watch(['dist/styles/*.css', 'dist/scripts/*.js'], livereload.changed);
   gulp.watch('src/styles/**', ['styles']);
-  gulp.watch(['src/scripts/**', '!src/scripts/bower_components/**'], ['lint-scripts', 'scripts:dist']);
+  gulp.watch(['src/scripts/**', '!src/scripts/bower_components/**'], ['scripts:dist']);
 });
 
-gulp.task('default', ['watch', 'livereload', 'styles', 'lint-scripts', 'scripts:dist']);
-gulp.task('build', ['styles', 'scripts', 'lint-scripts', 'scripts:dist']);
+gulp.task('default', ['watch', 'livereload', 'styles', 'scripts:dist']);
+gulp.task('build', ['styles', 'scripts', 'scripts:dist']);
